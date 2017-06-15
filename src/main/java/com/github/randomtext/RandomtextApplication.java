2c331c665cd1b448d3091c13b59fc829931ca6aa
Alexey Koptenkov <alexey.koptenkov@yandex.ru>
2017-06-15 15:23:51 +0300

    initial commit

---
+++ src/main/java/com/github/randomtext/RandomtextApplication.java
@@ -0,0 +1,12 @@
+package com.github.randomtext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomtextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomtextApplication.class, args);
+	}
+}
